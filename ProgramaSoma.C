fa76415995d7f7ae1851e91f4605f417ec157992
marcolas-67 <marcoantoniosousa2007@gmail.com>
2026-08-05 11:23:34 -0300

    Soma, fiz durante uma aula... agora fica aí pro caso do professor pedir
    Programa que soma dois números em C

---
+++ ProgramaSoma.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+int a, b, soma;
+
+  printf("escreva um numero...\n");
+  scanf("%d", &a);
+  
+  printf("escreva outro numero...\n");
+  scanf("%d", &b);
+  
+  soma = a + b;
+  printf("a soma dos numeros e: %d\n", soma);
+}
